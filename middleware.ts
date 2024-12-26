9a4dfb1008d8f8674ec1950f3dfad7ac62fdf6d5
Darshan4518 <codewithdarshan45@gmail.com>
2024-12-26 13:30:10 +0530

    icon changed

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(request: NextRequest) {
   const cookiesStore = await cookies();
   const token = cookiesStore.get("authToken")?.value;
 
-  const protectedPaths = ["/profile", "/chat", "/askgenie"];
+  const protectedPaths = ["/profile", "/chat", "/askgenie", "/"];
   const currentPath = request.nextUrl.pathname;
 
   const isProtectedPath = protectedPaths.some((path) =>
@@ -16,12 +16,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(loginUrl);
   }
 
-  if (
-    !token &&
-    (currentPath === "/chat" ||
-      currentPath === "/profile" ||
-      currentPath === "/")
-  ) {
+  if (!token && currentPath === "/") {
     const loginUrl = new URL("/login", request.url);
     return NextResponse.redirect(loginUrl);
   }
